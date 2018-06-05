8f3dcbc6347432b7b4e42dd38a13edb5e6f6909b
Daniel <d.andres.castillo.olivares@gmail.com>
2018-06-05 13:07:31 +0200

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -21,7 +21,7 @@ if (process.env.NODE_ENV == 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
     //Tenemos que copiar la segunda cadena de conexión de Azure
-    urlDB = 'mongodb://node:LPi7nMl0vtznOVozphb1Nozlm4BJyWQhyz5JWpAIJHjNnDwqyzXPRSqm7kakLXPfUeruesoGs5EdarKtOtLusA%3D%3D@node.documents.azure.com:10255/?ssl=true';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
